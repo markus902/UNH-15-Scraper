760f49845d0699aff29faaca48b11fa5e5ad634a
Markus Markowich <markus902@gmx.de>
2019-10-31 22:34:23 -0400

    initial commit, handlebars initialized

---
+++ server.js
@@ -0,0 +1,114 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars")
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb+srv://markus902:dasser@markuscluster-dmvcb.mongodb.net/articleScraper?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Handlebars Config
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Routes
+
+app.get("/index", function (req, res) {
+    res.render("index")
+})
+
+app.get("/scrape", function (req, res) {
+    axios.get("http://www.echojs.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("article h2").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    console.log("articles found")
+    db.Article.find({}).then(function (allArticles) {
+        console.log(allArticles);
+        res.json(allArticles);
+    }).catch(function (err) {
+        res.json(err)
+    });
+});
+
+app.get("/articles/:id", function (req, res) {
+    var id = req.params.id;
+    console.log("Article " + req.params.id);
+    db.Article.findOne({
+        _id: req.params.id
+    })
+        // .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        }).catch(function (err) {
+            res.json(err)
+        })
+});
+
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({
+                _id: req.params
+            }, { note: dbNote._id }, { new: true })
+        }).then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
