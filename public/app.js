f45f984c4405153551791b1f18721d4941a26350
Markus Markowich <markus902@gmx.de>
2019-11-03 22:43:35 -0500

    added frontend styling

--- public/app.js
+++ public/app.js
@@ -1,13 +1,28 @@
+// import { get } from "https";
+
+// import { WSAEINVALIDPROVIDER } from "constants";
 
 $(document).ready(function () {
 
   $("#noteBox").hide();
 
   $.getJSON("/articles", function (data) {
+    let counter = 0
     data.forEach(element => {
       let newArticleHeadline = $("<h2>");
+      let newDivy = $("<div>");
       let newArticlelink = $("<a>");
       let newNoteButton = $("<button>");
+
+      counter++;
+      console.log(counter)
+
+      if (counter % 2 == 0) {
+        newDivy.css("background", "rgb(182, 196, 227)");
+      } else {
+        newDivy.css("background", "white");
+      }
+      newDivy.addClass("newDivy p-3")
 
       newArticleHeadline
         .addClass("articleHeadline")
@@ -15,19 +30,32 @@ $(document).ready(function () {
         .attr("value", element._id);
 
       newArticlelink
-        .addClass("articleLink")
+        .addClass("articleLink pl-4 pr-4")
         .text("Go to page ...")
         .attr("href", element.link);
 
-      newNoteButton.addClass("noteButton btn btn-primary")
+      newNoteButton.addClass("noteButton btn btn-primary pl-4 pr-4")
         .attr("value", element._id)
         .text("Note");
 
-      $("#articles")
+      newDivy
         .append(newArticleHeadline)
         .append(newArticlelink)
         .append(newNoteButton);
+
+      $("#articles")
+        .append(newDivy);
     });
+    counter = 0;
+  });
+
+  $("#scrapeButton").on("click", function () {
+    $.getJSON("/scrape", function (data) {
+      // console.log(data);
+    }).then(function (data) {
+      // console.log(data);;
+    });
+    location.reload(true)
   });
 
   $(document).on("click", ".noteButton", function () {
@@ -42,19 +70,22 @@ $(document).ready(function () {
     })
       .then(function (data) {
         console.log(data);
-
-        $("#notes").append(newForm).append(newSaveButton);
-
         if (data.note) {
-          $("#titleinput").val(data.note.title);
-          $("#bodyinput").val(data.note.body);
-        }
+          data.note.map((element) => {
+            let newNote = $("<li>");
+            newNote.text(element);
+            $("#existingNotes").append(newNote);
+          });
+        } else {
+          newNote.text("There are no notes for this article.")
+          $("#existingNotes").append(newNote)
+        };
       });
   });
 
   $(document).on("click", "#saveNote", function () {
     var thisId = $(this).attr("value");
-    // console.log(thisId, $("#noteTitleInput").val(), $("#noteBodyInput").val())
+    console.log(thisId, $("#noteTitleInput").val(), $("#noteBodyInput").val())
 
     $.ajax({
       method: "POST",
@@ -62,15 +93,15 @@ $(document).ready(function () {
       data: {
         title: $("#noteTitleInput").val(),
         body: $("#noteBodyInput").val(),
+        id: thisId
       }
     })
       .then(function (data) {
-        console.log(data);
         $("#notesTitleInput").empty();
         $("#notesBodyInput").empty();
+        $("#existingNotes").empty();
       });
 
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
+    $("#noteBox").hide();
   });
 });
